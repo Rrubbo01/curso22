56e9f20dee8ac76de2af5bb0651074324715a290
Rrubbo01 <rodrigo.rubbo@gmail.com>
2022-04-12 22:10:19 -0300

    Variaveis do PHP

--- usuario.php
+++ usuario.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Olá mundo.";
+echo "Olá mundo. <br><br>";
+
+$numero = 5;
+$resto = 2;
+
+$resto = $numero % 2;
+if ($resto == 0) {
+    echo "O número {$numero} é par.";
+} else {
+    echo "O número ".$numero . " é impar.";
+}
