5d827b8b501fa7b3f4d90edd8f7a56be87baab1a
Rrubbo01 <rodrigo.rubbo@gmail.com>
2022-04-19 22:05:44 -0300

    Laços

--- usuario.php
+++ usuario.php
@@ -2,8 +2,8 @@
 
 echo "Olá mundo. <br><br>";
 
-$numero = 5;
-$resto = 2;
+$numero = 4;
+$resto = 0;
 
 $resto = $numero % 2;
 if ($resto == 0) {
@@ -11,3 +11,42 @@ if ($resto == 0) {
 } else {
     echo "O número ".$numero . " é impar.";
 }
+
+$soma = 0;
+$valor1 = 10;
+$valor2 = 20;
+
+$valor = $valor1 + $valor2;
+
+$soma1 = $soma + 29;
+
+$soma +=29;
+
+echo "<br><br> A soma dos números {$valor1} + {$valor2} é: <br> soma:{$soma} <br> soma1: {$soma1}.";
+
+
+
+switch($resto) {
+    case 0 : echo "O número {$numero} é par. <br>";
+        break;
+    default: echo " O número `{$número} é impar. <br>";
+        break;
+};
+
+echo "<br><br><br>Laço for.<br><br><br>";
+
+
+$resto = 0;
+$numero = [4, 2, 5, 1, 10, 100, 50, 3, 9, 12];
+$resto = $numero % 2;
+$contpares = 0;
+$i = 0;
+
+//for ($i = 0; $contpares < 5; $i++){
+//$resto = $numero[$i] % 2
+//    if ($resto == 0) {
+//    $contpares++;        
+//    }
+//}
+
+echo "Data hora início: " . date("d/m/y H:i:s");
