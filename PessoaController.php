23ae49aca6e76718d9a86c69da64e4481b61afb1
Rrubbo01 <101676249+Rrubbo01@users.noreply.github.com>
2022-05-17 22:06:09 -0300

    Create PessoaController.php

---
+++ PessoaController.php
@@ -0,0 +1,17 @@
+<?php
+
+class PessoaController {
+    $pessoa = new Pessoa();
+
+    if ($_POST['cpf'] != '') {
+        $pessoaFisica = new PessoaFisica();
+        $pessoaFisica->CPF = $_POST['cpf'];
+        $pessoa->salvar($pessoaFisica);
+    } else {
+        $pessoaJuridica = new PessoaJuridica();
+        $pessoaJuridica->CNPJ = $_POST['CNPJ'];
+        $pessoa->salvar($pessoaJuridica);
+
+    }
+
+}
