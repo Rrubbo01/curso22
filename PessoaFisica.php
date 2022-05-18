103d1a04eef5cd6a613305ff35a7e680406e2ecf
Rrubbo01 <101676249+Rrubbo01@users.noreply.github.com>
2022-05-17 22:04:05 -0300

    Create PessoaFisica.php

---
+++ PessoaFisica.php
@@ -0,0 +1,25 @@
+<?php
+class PessoaFisica extends IPessoa{
+    public $CPF = "";
+    public $Estado_Civil = "";
+    public $genero = "";
+    
+
+    public function salvar() {
+
+    }
+    public function obter(){
+
+    }
+    public function ObterTodas(){
+
+    }
+    public function atualizar(){
+
+    }
+    public function excluir(){
+
+    }
+
+
+}
