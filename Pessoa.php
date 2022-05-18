7f1d50a365b750b6902b3eea3505516c82fde296
Rrubbo01 <101676249+Rrubbo01@users.noreply.github.com>
2022-05-17 22:04:43 -0300

    Create Pessoa.php

---
+++ Pessoa.php
@@ -0,0 +1,28 @@
+<?php
+class Pessoa implements IPessoa{
+    public $id = 0;
+    public $Nome = "";
+    public $CEP = 0;
+    public $Email = "";
+    public $celular = "";
+    public $status = 0;
+
+
+    public function salvar($dados) {
+
+    }
+    public function obter(){
+
+    }
+    public function ObterTodas(){
+
+    }
+    public function atualizar(){
+
+    }
+    public function excluir(){
+
+    }
+
+
+}
